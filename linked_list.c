fb917644f1b68a448bc5d58f1c8dce14bbd0525b
zhalliga <156023800+zhalliga@users.noreply.github.com>
2025-04-04 18:29:13 -0400

    initial commit

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,89 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) { 
+  struct list_node *node = malloc(sizeof(struct list_node));
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+  if(node == NULL){
+    return NULL; 
+  }
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+  node->value = value;
+  node->next = NULL;
+  return node;
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node *new = new_node(value);
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+  if(new == NULL){
+    return NULL;
+  }
 
-void free_list(struct linked_list list) {}
+  new->next = list->head;
+  list->head = new;
+}
+
+void insert_at_tail(struct linked_list *list, size_t value) {
+  struct list_node *new = new_node(value);
+
+  if(new == NULL){
+    return NULL;
+  }
+  if(list->head == NULL){
+    return NULL;
+  }
+  struct list_node *current = list->head;
+  while(current->next != NULL){
+    current = current->next;
+  }
+  current->next = new;
+}
+
+size_t remove_from_head(struct linked_list *list) { 
+  if(list->head == NULL){
+    return 0;
+  }
+
+  struct list_node *node = list->head;
+  size_t value = node->value;
+  list->head = node->next;
+  free(node);
+
+  return value; 
+}
+
+size_t remove_from_tail(struct linked_list *list) { 
+
+  if(list->head == NULL){
+    return 0;
+  }
+
+  if(list->head->next == NULL){
+    size_t value = list->head->value;
+    free(list->head);
+    list->head = NULL;
+    return(value);
+  }
+
+  struct list_node *node = list->head;
+  while((node->next != NULL) && (node->next->next != NULL)){
+    node = node->next;
+  }
+
+  size_t value = node->next->value;
+  free(node->next);
+  node->next = NULL;
+  return value; 
+}
+
+void free_list(struct linked_list list) {
+  while(list.head != NULL){
+    struct list_node *node = list.head;
+    list.head = list.head->next;
+    free(node);
+  }
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,46 @@
 #include "queue.h"
 #include "tile_game.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+    uint64_t state_val = serialize(state);
+    insert_at_tail(&q->data, state_val);
 
-int number_of_moves(struct game_state start) { return 0; }
+}
+
+struct game_state dequeue(struct queue *q) { 
+
+    uint64_t state_val = remove_from_head(&q->data);
+    return deserialize(state_val);
+
+}
+
+int number_of_moves(struct game_state start) { 
+    struct queue q = {0}; //initialize q structure
+    enqueue(&q, start);
+
+    while(q.data.head != NULL){
+        struct game_state current = dequeue(&q);
+        if(checker_function(current)){
+            return current.num_steps;
+        }
+        else{
+            
+        }
+    }
+
+    return 0; 
+}
+
+bool checker_function (struct game_state state){
+    uint8_t goal[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}};
+
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            if(state.tiles[i][j] != goal[i][j]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
